ad6afcabf697beaa9d32a39309105e4d0fc79438
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-01-24 21:45:14 +0530

    Update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,7 +7,7 @@ const Search = () => {
     <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3 gap-4 px-4 py-16'>
       <div className='lg:col-span-2 flex flex-col justify-evenly'>
         <div>
-          <h2>LUXURY INCLUDED VACATIONS FOR TWO PEOPLE</h2>
+          <h2 className='mb-3'>LUXURY INCLUDED VACATIONS FOR TWO PEOPLE</h2>
           <p className='py-4'>
             Come experience the very pinnacle of luxury Caribbean all-inclusive
             vacations for couples at BEACHES Resorts. Our luxury beach resorts,
@@ -27,7 +27,7 @@ const Search = () => {
             </button>
             <div>
                 <h3 className='py-2'>LEADING SERVICE</h3>
-                <p className='py-1'>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</p>
+                <p className='px-1'>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</p>
             </div>
           </div>
           <div className='flex flex-col lg:flex-row items-center text-center'>
@@ -36,14 +36,14 @@ const Search = () => {
             </button>
             <div>
                 <h3 className='py-2'>LEADING SERVICE</h3>
-                <p className='py-1'>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</p>
+                <p className='px-1'>ALL-INCLUSIVE COMPANY FOR 20 YEARS IN-A-ROW</p>
             </div>
           </div>
         </div>
       </div>
 
       <div>
-          <div className='border text-center'>
+          <div className='border text-center rounded-lg'>
               <p className='pt-2'>GET AN ADDITIONAL 10% OFF</p>
               <p className='py-4'>12 HOURS LEFT</p>
               <p className='bg-gray-800 text-gray-200 py-2'>BOOK NOW AND SAVE</p>
