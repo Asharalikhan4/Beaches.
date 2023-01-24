92e49a52fc251aa599409d15de9c154e0c2e35b2
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-01-24 21:33:44 +0530

    Destinations added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,41 @@
-import React from "react";
-import maldivesVideo from "../assets/maldivesVideo.mp4";
-import { AiOutlineSearch } from "react-icons/ai";
+import React from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import maldivesVideo from '../assets/maldivesVideo.mp4';
 
 const Hero = () => {
   return (
-    <div className="w-full h-screen relative">
-        <video className="w-full h-full object-cover" src={maldivesVideo} autoPlay loop muted/>
-        <div className="absolute w-full h-full left-0 bg-gray-900/30"></div>
-            <div className="absolute top-0 w-full h-full flex flex-col justify-center text-white text-center p-4">
-                <h1>First Class Travel</h1>
-                <h2 className="py-3">Top 1% Locations WorldWide</h2>
-                <form className="flex justify-between items-center max-w-[700px] mx-auto w-full border p-1 rounded-md text-black bg-gray-100/90">
-                    <div>
-                    <input className="bg-transparent w-[300px] h-[40px] sm:w-[400px] font-[Poppins] focus:outline-none" type="text" placeholder="Search Destinations" />
-                    </div>
-                    <div>
-                        <button><AiOutlineSearch size={20} style={{color: "#ffffffff"}} className="icon" /></button>
-                    </div>
-                </form>
-            </div>
-        </div>
+    <div className='w-full h-screen relative'>
+      <video
+        className='w-full h-full object-cover'
+        src={maldivesVideo}
+        autoPlay
+        loop
+        muted
+      />
+      <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
+      <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
+        <h1>First Class Travel</h1>
+        <h2 className='py-4'>Top 1% Locations Worldwide</h2>
+        <form
+          className='flex justify-between items-center max-w-[700px] mx-auto w-full border p-1
+          rounded-md text-black bg-gray-100/90'
+        >
+          <div>
+            <input
+              className='bg-transparent w-[300px] h-[40px] sm:w-[400px] font-[Poppins] focus:outline-none'
+              type='text'
+              placeholder='Search Destinations'
+            />
+          </div>
+          <div>
+            <button>
+              <AiOutlineSearch size={20} className='icon' style={{color: '#ffffff'}}  />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Hero
+export default Hero;
